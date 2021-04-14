54b5fc56ad4cd969a9fa584e49762ccd4c2a5842
grif <gmpaier@loyola.edu>
2021-04-14 18:24:25 -0400

    Began filling out functions with actually queries. Assignment due tonight, so I may have to hold off on the bonuses.

--- server.js
+++ server.js
@@ -16,37 +16,167 @@ const connection = mysql.createConnection({
 });
 
 const welcome = () => {
-
+  inquirer
+  .prompt({
+    name: 'route',
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: ['VIEW', 'ADD', 'UPDATE', 'DELETE', 'EXIT'],
+  })
+  .then((answer) => {
+    switch(answer.route){
+      case 'VIEW':
+        return viewOpt();
+      case 'ADD':
+        return addOpt();
+      case 'UPDATE':
+        return updateOpt();
+      case 'DELETE':
+        return delOpt();
+      case 'EXIT':
+        console.log("Goodbye");
+        connection.end();
+    }
+  });
 }
 
 const viewOpt = () => {
-
+  inquirer
+  .prompt({
+    name: 'route',
+    type: 'list',
+    message: 'What would you like to view?',
+    choices: ['VIEW DEPARTMENT', 'VIEW ROLES', 'VIEW EMPLOYEES', 'BACK'],
+  })
+  .then((answer) => {
+    switch(answer.route){
+      case 'VIEW DEPARTMENT':
+        return viewDept();
+      case 'VIEW ROLES':
+        return viewRoles();
+      case 'VIEW EMPLOYEES':
+        return viewEmployees();
+      case 'BACK':
+        return welcome();
+    }
+  });
 }
 
 const viewDept = () => {
-
+  connection.query('SELECT * FROM department;', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    welcome();
+  })
 }
 
 const viewRoles = () => {
-
+  connection.query('SELECT role.title as Title, role.salary as Salary, department.name AS Department FROM role JOIN department ON role.department_id = department.id;', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    welcome();
+  })
 }
 
 const viewEmployees = (opt) => {
-// switch(opt)
-// case "viewByManager"
-// default --"viewAll"
+  connection.query('SELECT a.id, a.first_name AS First, a.last_name AS Last, role.title as Title, role.salary as Salary, CONCAT(b.first_name, " ", b.last_name) AS Manager FROM employee a JOIN employee b on a.manager_id = b.id JOIN role ON a.role_id = role.id;', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    welcome();
+  })
 }
 
-const addOpt = () => {
 
+const addOpt = () => {
+  inquirer
+  .prompt({
+    name: 'route',
+    type: 'list',
+    message: 'What would you like to add?',
+    choices: ['ADD NEW DEPARTMENT', 'ADD NEW ROLE', 'ADD NEW EMPLOYEE', 'BACK'],
+  })
+  .then((answer) => {
+    switch(answer.route){
+      case 'ADD NEW DEPARTMENT':
+        return addDept();
+      case 'ADD NEW ROLE':
+        return addRole();
+      case 'ADD NEW EMPLOYEE':
+        return addEmployee();
+      case 'BACK':
+        return welcome();
+    }
+  });
 }
 
 const addDept = () => {
-
+  inquirer
+  .prompt({
+    name: 'name',
+    message: 'What is the name of your new department?' 
+  })
+  .then((answer) => {
+    connection.query('INSERT INTO department SET ?',
+    {
+      name: answer.name
+    },
+    (err) => {
+      if (err) throw err;
+      console.log(`${answer.name} added to departments.`);
+      viewDept();
+    }
+    )
+  })
 }
 
 const addRole = () => {
-
+  connection.query('SELECT name FROM deparment', (err, results) => {
+    if (err) throw err;
+    inquirer
+    .prompt([
+      {
+        name: 'title',
+        message: 'What is the name of your new role?' 
+      },
+      {
+        name: 'salary',
+        message: 'What is the salary amount for this role?',
+        validate(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        type: "list",
+        name: 'department',
+        message: 'Which department does this role belong to?',
+        choices(){
+          const choiceArray = [];
+          results.forEach(({name}) => {
+            choiceArray.push(name);
+          });
+          return choiceArray;
+        },
+      }
+    ]).then((answer) => { 
+      connection.query(`SELECT id FROM department WHERE name = ${answer.list}`, (err, results) => {
+        if (err) throw err;
+        connection.query('INSERT INTO role SET ?', 
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: results[0].id
+        },
+        (error) => {
+          if (error) throw err;
+          console.log(`${answer.title} successfully added to roles.`)
+          viewRoles();
+        });
+      });
+    });
+  });
 }
 
 const addEmployee = () => {
@@ -54,7 +184,23 @@ const addEmployee = () => {
 }
 
 const updateOpt = () => {
-
+  inquirer
+  .prompt({
+    name: 'route',
+    type: 'list',
+    message: 'What would you like to update?',
+    choices: ['UPDATE EMPLOYEE ROLE', 'UPDATE EMPLOYEE MANAGER', 'BACK'],
+  })
+  .then((answer) => {
+    switch(answer.route){
+      case 'UPDATE EMPLOYEE ROLE':
+        return updateEmployee(role);
+      case 'UPDATE EMPLOYEE MANAGER':
+        return updateEmployee(man);
+      case 'BACK':
+        return welcome();
+    }
+  });
 }
 
 const updateEmployee = (opt) => {
@@ -62,7 +208,25 @@ const updateEmployee = (opt) => {
 }
 
 const delOpt = () => {
-
+  inquirer
+  .prompt({
+    name: 'route',
+    type: 'list',
+    message: 'What would you like to delete?',
+    choices: ['DELETE DEPARTMENT', 'DELETE ROLE', 'DELETE EMPLOYEE', 'BACK'],
+  })
+  .then((answer) => {
+    switch(answer.route){
+      case 'DELETE DEPARTMENT':
+        return addDept();
+      case 'DELETE ROLE':
+        return addRole();
+      case 'DELETE EMPLOYEE':
+        return addEmployee();
+      case 'BACK':
+        return welcome();
+    }
+  });
 }
 
 const delDept = () => {
@@ -83,6 +247,6 @@ const fullName = (first, last) => {
 
 connection.connect((err) => {
   if (err) throw err;
-  
+  console.log("Welcome to the Employee Tracker program. \n")
   welcome();
 });
